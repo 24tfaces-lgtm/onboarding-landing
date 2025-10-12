116aa8eb68484180eb36bbe8d467164782118e92
24tfaces-lgtm <24tfaces@gmail.com>
2025-10-12 10:51:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Пример для плавных переходов по секциям
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});

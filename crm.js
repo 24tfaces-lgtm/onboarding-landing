3a50f7f8fecdf4cd33187ec6e143d9c819fea4bd
24tfaces-lgtm <24tfaces@gmail.com>
2025-10-16 14:16:39 +0500

    Add files via upload

---
+++ crm.js
@@ -0,0 +1,10 @@
+// Smooth scroll for internal anchor links
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', e => {
+    const id = a.getAttribute('href');
+    if (id && id.length > 1) {
+      e.preventDefault();
+      document.querySelector(id)?.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});

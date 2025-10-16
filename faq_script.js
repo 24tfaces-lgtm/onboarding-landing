350ee855f39065df7e8484ea586f70d0e7ee1f0a
24tfaces-lgtm <24tfaces@gmail.com>
2025-10-16 11:51:53 +0500

    Add files via upload

---
+++ faq_script.js
@@ -0,0 +1,9 @@
+// Открывать вопрос по хэшу из URL (например, faq.html#q-fines)
+const openByHash = () => {
+  const id = location.hash.replace('#','');
+  if (!id) return;
+  const el = document.getElementById(id);
+  if (el && el.tagName.toLowerCase() === 'details') el.setAttribute('open', '');
+};
+window.addEventListener('hashchange', openByHash);
+openByHash();
